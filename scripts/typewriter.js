c449e4a14c397069fbd1c12cc9512c3fdf435dd7
Felix Maury <femaury@student.42.fr>
2020-02-29 17:57:00 +0100

    Updated website.

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -1,42 +1,67 @@
-var adjList = ["style", "efficiency", "innovation", "passion"]
-var adjective = document.getElementById('adj');
-var index = -1;
-var i = 0;
-var adj = adjList[0];
-
-function writeWord() {
-  index++;
-  if (index === adjList.length) { index = 0 }
-  adjective.textContent = '';
-  adj = adjList[index];
-  writeChars();
-}
-
-function unwriteChars() {
+function unwriteChars(element, word, i) {
   document.getElementById('blinker').className = '';
-  if (i > -1) {
-    adjective.textContent = adj.substring(0, i);
-    i--;
-    setTimeout(unwriteChars, 50);
+  if (i >= 0) {
+    element.textContent = word.substring(0, i);
+    setTimeout(function () {
+      i--;
+      unwriteChars(element, word, i);
+    }, 50);
   } else {
-    writeWord();
-  }
-}
-
-function writeChars() {
-  document.getElementById('blinker').className = '';
-  if (i < adj.length) {
-    adjective.textContent += adj.charAt(i);
-    i++;
-    setTimeout(writeChars, 75);
-  } else {
-    setTimeout(function() {
+    setTimeout(function () {
       document.getElementById('blinker').className = 'blinker';
     }, 200);
-    setTimeout(unwriteChars, 2000);
   }
 }
 
-onload=function() {
-  writeWord()
+function write(element, word, i = 0, erase, eraseDelay) {
+  document.getElementById('blinker').className = '';
+  if (i < word.length) {
+    element.textContent += word.charAt(i);
+    setTimeout(function () {
+      i++;
+      write(element, word, i);
+    }, 90);
+  } else {
+    setTimeout(function () {
+      document.getElementById('blinker').className = 'blinker';
+    }, 200);
+    if (erase) {
+      setTimeout(function () {
+        unwriteChars(element, word, word.length);
+      }, eraseDelay);
+    }
+  }
+}
+
+function addText(parent, text) {
+  const div = document.createElement('div');
+  div.innerHTML = text;
+  parent.appendChild(div);
+}
+
+function addLine(parent, divID) {
+  const div = document.createElement('div');
+  div.innerHTML = `felix@femaury.dev <span class="blue">~</span> <span class="orange">❯</span> <span id="${divID}"></span><span id="blinker" class="blinker">|</span>`;
+  parent.appendChild(div);
+}
+
+onload = function () {
+  const terminal = this.document.getElementById('terminal-text');
+  const elem1 = document.getElementById('cat-readme');
+  this.setTimeout(function() {
+    write(elem1, "cat README.txt");
+  }, 1000);
+  this.setTimeout(function() {
+    document.getElementById('blinker').remove();
+    addText(terminal, `
+      Hey there!<br />
+      <br />
+      This website is still under construction...<br />
+      but if you want to get in touch with me, you can send me a mail!<br />
+      <a target="_blank" href="mailto:business@femaury.dev">business@femaury.dev</a><br />
+      Cheers!<br />
+      <br />
+    `);
+    addLine(terminal);
+  }, 2500);
 };
